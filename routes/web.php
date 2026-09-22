136e78113e34828c8f652fd30e7ffb62724b3eb7
Your Name <you@example.com>
2026-09-22 09:30:02 +0800

    perubahan royyan

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,16 @@ Route::get('/', function () {
 });
 
 Route::get('/danish', function () {
+    return view('welcome');
 });
 
 Route::get('/Qourta', function () {
     return view('welcome');
 });
+
+Route::get('/Royyan', function () {
+    return view('welcome');
+});
+
+git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
